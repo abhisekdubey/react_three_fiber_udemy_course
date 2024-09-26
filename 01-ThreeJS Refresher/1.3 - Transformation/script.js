055be41ba3ba8189afc62c964a0bd99b141e7b15
Abhisek Dubey <abhidb95@gmail.com>
2024-09-26 16:42:06 +0530

    Lect : 1.3 Transformation done

--- 01-ThreeJS Refresher/1.3 - Transformation/script.js
+++ 01-ThreeJS Refresher/1.3 - Transformation/script.js
@@ -4,13 +4,37 @@ import * as THREE from "./three.module.min.js";
 // Scene
 const scene = new THREE.Scene();
 
+// Group
+const group = new THREE.Group();
+
 // Mesh
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: "purple" });
 
 const mesh = new THREE.Mesh(geometry, material);
+mesh.position.x = 1;
+// mesh.position.y = 1;
+// mesh.position.z = 1;
+// mesh.scale.x = 2;
+// mesh.scale.y = 3;
+// mesh.rotation.x = Math.PI * 0.25;
+// mesh.rotation.y = Math.PI * 1.2;
+// scene.add(mesh);
 
-scene.add(mesh);
+// Mesh two
+const geometryT = new THREE.BoxGeometry(1, 1, 1);
+const materialT = new THREE.MeshBasicMaterial({ color: "green" });
+const meshT = new THREE.Mesh(geometryT, materialT);
+meshT.position.y = 2;
+// scene.add(meshT);
+
+group.add(mesh, meshT);
+group.position.x = 3;
+scene.add(group);
+
+// Axes helper
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 // Camera
 const aspect = {
