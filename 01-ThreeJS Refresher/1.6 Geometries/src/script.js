228072b991bd4cd7b3437ae06ce28f0c6e5a9e62
Abhisek Dubey <abhidb95@gmail.com>
2024-09-27 12:46:09 +0530

    Lect : 1.6 Geometries done

---
+++ 01-ThreeJS Refresher/1.6 Geometries/src/script.js
@@ -0,0 +1,64 @@
+// Scene Objects-(Mesh) Camera Renderer
+import * as THREE from "three";
+
+// Scene
+const scene = new THREE.Scene();
+
+// Mesh
+const geometry = new THREE.PlaneGeometry(1, 1);
+// const geometry = new THREE.BufferGeometry();
+
+// const verticesArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+
+// const positionAttribute = new THREE.BufferAttribute(verticesArray, 3);
+// geometry.setAttribute("position", positionAttribute);
+
+const material = new THREE.MeshBasicMaterial({
+  color: "purple",
+  wireframe: true,
+});
+
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+
+// Camera
+const aspect = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+const camera = new THREE.PerspectiveCamera(75, aspect.width / aspect.height); // near value is 1 and far value is 2000
+camera.position.z = 3;
+scene.add(camera);
+
+// Renderer
+const canvas = document.querySelector("#canvas"); // Select the Canvas element
+const renderer = new THREE.WebGLRenderer({ canvas }); // add the WebGLRenderer
+renderer.setSize(aspect.width, aspect.height); //Renderer Size
+
+// Clock Class
+const clock = new THREE.Clock();
+
+// Animate
+const animate = () => {
+  // GetElapsedTime
+  const elapsedTime = clock.getElapsedTime();
+
+  // Update Rotation On X Axes
+  mesh.rotation.y = elapsedTime * Math.PI;
+
+  // mesh.rotation.x += 0.01;
+  // mesh.position.x += 0.01;
+  // mesh.position.y += 0.01;
+
+  // Renderer
+  renderer.render(scene, camera); // Display what the camera in the scene captured
+
+  // Request ANimation Frame
+  window.requestAnimationFrame(animate);
+};
+
+animate();
+
+// function will get called 60 times per second on some devices 0.1 * 60 => 0.6 X
+// function will get called 120 times per second on some devices 0.1 * 120 => 1.2 X
+// fps - stands for frame Per Second
