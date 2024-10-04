a78f0ee3642f761fd7147bc12574fb66bdcfa206
Abhisek Dubey <abhidb95@gmail.com>
2024-10-04 15:16:56 +0530

    Performance: we used r3f-perf for gpu performnace

--- 03-React Three Fiber/r_3_f/src/App.js
+++ 03-React Three Fiber/r_3_f/src/App.js
@@ -8,6 +8,7 @@ import {
   ParticlesScene,
   Scene,
 } from "./components";
+import { Perf } from "r3f-perf";
 
 // const createdCanvasHandler = (state) => {
 //   console.log("Canvas has been created !");
@@ -16,24 +17,30 @@ import {
 
 const App = () => {
   return (
-    <Canvas
-      gl={{ antialias: true, alpha: true }}
-      // orthographic
-      camera={{
-        fov: 75,
-        near: 0.5,
-        far: 100,
-        // zoom: 120,
-        position: [2, 2, 6],
-      }}
-      // onCreated={createdCanvasHandler}
-    >
-      {/* <Scene /> */}
-      {/* <AxesHelperScene /> */}
-      {/* <CustomGeometryScene /> */}
-      {/* <LoaderScene /> */}
-      {/* <ParticlesScene /> */}
+    // <Canvas
+    //   gl={{ antialias: true, alpha: true }}
+    //   // orthographic
+    //   camera={{
+    //     fov: 75,
+    //     near: 0.5,
+    //     far: 100,
+    //     // zoom: 120,
+    //     position: [2, 2, 6],
+    //   }}
+    //   // onCreated={createdCanvasHandler}
+    // >
+    //   {/* <Scene /> */}
+    //   {/* <AxesHelperScene /> */}
+    //   {/* <CustomGeometryScene /> */}
+    //   {/* <LoaderScene /> */}
+    //   {/* <ParticlesScene /> */}
+    //   <Loading3DModel />
+    // </Canvas>
+
+    // r3f-perf
+    <Canvas>
       <Loading3DModel />
+      <Perf position="top-left" />
     </Canvas>
   );
 };
